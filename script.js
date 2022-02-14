89f1cd2ec84ec0b603a12a27d9bccca4c8a3b3ba
Jaxo <87024826+JaxoDevOfficial@users.noreply.github.com>
2022-02-14 17:16:10 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+(function(){
+    var oldLog = console.log;
+    console.log = function (message) {
+        document.getElementById('a').value += message
+        oldLog.apply(arguments);
+    };
+})();
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+var textarea = document.getElementById('a')
+if (navigator.userAgent.indexOf('Mobile') !== -1) { document.getElementById('save').style.display = "none"; textarea.style.height = "100px";
+}//resize for mobile
+function generate() {
+var triesPerSecond = document.getElementById('speed').value //self explanatory
+getGiftCode = function () {
+    let code = '';
+    let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    for(var i = 0; i < 16; i++){
+        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
+    }
+    console.log('[GEN] http://discord.gift/' + code + "\n");
+
+}
+getGiftCode();
+function stop() {
+  clearInterval(gInterval);
+  clearInterval(interval)
+}
+var gInterval = setInterval(() => {getGiftCode();}, (1/triesPerSecond) * 1000);
